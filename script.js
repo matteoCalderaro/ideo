5d757c297fb58a82ace69c2a3302a64de234ff60
Matteo Calderaro <matteocalderaro@gmail.com>
2023-06-07 14:14:01 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+// this substitutes the the css :target and :checked which open/close the navbar and the popup
+
+const allLinks = document.querySelectorAll('a:link');
+const checkbox = document.querySelector('#navi-toggle');
+const popup = document.querySelector('#popup');
+
+allLinks.forEach((link) => {
+  const href = link.getAttribute('href');
+
+  if (href === '#') {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+  }
+  if (href !== '#' && href.startsWith('#') && href !== '#popup') {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      const section = document.querySelector(href);
+      section.scrollIntoView({ behavior: 'smooth' });
+      checkbox.checked = false;
+      popup.style.visibility = 'hidden';
+    });
+  }
+  if (href == '#popup') {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      popup.style.visibility = 'visible';
+      popup.style.opacity = '1';
+      popup.firstElementChild.style.opacity = '1';
+      popup.firstElementChild.style.transform =
+        'translate(-50%, -50%) scale(1)';
+    });
+  }
+});
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+const header = document.querySelector('.header__text-box');
+
+const obs = new IntersectionObserver(
+  (entries) => {
+    const ent = entries[0];
+    console.log(ent);
+    const button = document.querySelector('.navigation__button');
+    const background = document.querySelector('.navigation__background');
+    const nav = document.querySelector('.navigation__nav');
+    if (ent.isIntersecting === false) {
+      button.style.display = 'block';
+      background.style.display = 'block';
+      nav.style.display = 'block';
+    }
+    if (ent.isIntersecting === true) {
+      button.style.display = 'none';
+      background.style.display = 'none';
+      nav.style.display = 'none';
+    }
+  },
+  {
+    // In the viewport (null means viewport)
+    root: null,
+    threshold: 0,
+    rootMargin: '-150px',
+  }
+);
+obs.observe(header);
+
+// navigation__button;
+// navigation__background;
+// navigation__nav;
